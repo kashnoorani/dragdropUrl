8b59edfd7698a85098851fd984f2861c656bc9b7
Kash Noorani <2106211+kashnoorani@users.noreply.github.com>
2024-10-05 07:09:19 -0700

    Delete build directory

--- build/DragAndDropToOpenURL.xcarchive/Products/Applications/Drag and Drop to Open URL.app/Contents/PlugIns/Drag and Drop to Open URL Extension.appex/Contents/Resources/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'openInBackground') {
-    chrome.tabs.create({ url: request.url, active: false });
-  }
-});
